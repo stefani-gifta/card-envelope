07e967be3381d16d0d69cea0d82c6223fb50e4a1
gifta <152134013+stefani-gifta@users.noreply.github.com>
2025-07-13 12:11:51 +0000

    Add instruction

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 const env = document.getElementsByClassName('envelope');
 const flap = document.getElementsByClassName('envelope-flap');
 const card = document.getElementsByClassName('card');
-const xBtn = document.getElementsByClassName('card-close-button');
+const instruction = document.getElementById('instruction');
 
-flap[0].addEventListener('click', function() {
-    if(flap[0].classList.contains('envelope-flap-hover')) {
+instruction.innerHTML = "Click on envelope to open!";
+
+env[0].addEventListener('click', function() {
+    if(flap[0].classList.contains('envelope-flap-hover') && !card[0].classList.contains('card-peek') && !card[0].classList.contains('card-show')) {
+        // close envelope
         env[0].classList.remove('envelope-after');
         flap[0].classList.remove('envelope-flap-hover');
         card[0].classList.remove('card-show');
         card[0].classList.remove('card-hide');
-    } else {
+        instruction.innerHTML = "Click on envelope to open!";
+    } else if(!card[0].classList.contains('card-peek')) {
+        // open envelope + peek card
         env[0].classList.add('envelope-after');
         flap[0].classList.add('envelope-flap-hover');
         card[0].classList.add('card-peek');
+        instruction.innerHTML = "Click on card to open and close!";
     };
 });
 
 card[0].addEventListener('click', function() {
     if(card[0].classList.contains('card-peek')) {
+        // open card
         card[0].classList.remove('card-peek');
         card[0].classList.add('card-show');
     } else {
+        // hide card
         card[0].classList.remove('card-show');
         card[0].classList.add('card-hide');
+        instruction.innerHTML = "Click on envelope to close!";
     }
 });
