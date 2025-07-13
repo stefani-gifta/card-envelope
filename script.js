2b5a1b08f65586b4d53791602c38227b94622147
gifta <152134013+stefani-gifta@users.noreply.github.com>
2025-07-13 11:49:22 +0000

    Add reverse w/o close button

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const env = document.getElementsByClassName('envelope');
 const flap = document.getElementsByClassName('envelope-flap');
-const card = document.getElementsByClassName('card'); 
+const card = document.getElementsByClassName('card');
+const xBtn = document.getElementsByClassName('card-close-button');
 
 flap[0].addEventListener('click', function() {
-    env[0].classList.add('envelope-after');
-    flap[0].classList.add('envelope-flap-hover');
-    card[0].classList.add('card-peek');
+    if(flap[0].classList.contains('envelope-flap-hover')) {
+        env[0].classList.remove('envelope-after');
+        flap[0].classList.remove('envelope-flap-hover');
+        card[0].classList.remove('card-show');
+        card[0].classList.remove('card-hide');
+    } else {
+        env[0].classList.add('envelope-after');
+        flap[0].classList.add('envelope-flap-hover');
+        card[0].classList.add('card-peek');
+    };
 });
 
 card[0].addEventListener('click', function() {
-    card[0].classList.remove('card-peek');
-    card[0].classList.add('card-show');
+    if(card[0].classList.contains('card-peek')) {
+        card[0].classList.remove('card-peek');
+        card[0].classList.add('card-show');
+    } else {
+        card[0].classList.remove('card-show');
+        card[0].classList.add('card-hide');
+    }
 });
